9ec24a2a3948a934e991c736c128fac4ab3ecb6c
Lukas <lp.milestone@gmail.com>
2018-12-08 11:11:27 +0100

    init

---
+++ zadanie_1.py
@@ -0,0 +1,29 @@
+#policz pole trojkata o posdtawie 10 i wysokosci 5
+
+
+a = 10
+h= 5 
+wynik = 0.5 * a * h
+
+print("Pole trojkonta wynosi ", wynik)
+
+
+# Policz pole koła o promieniu 7
+
+r = 7
+wynik = 3.14 * r ** 2
+print("Pole koła", wynik)
+
+
+#policz pole trapezu o dlugosci podstawy 3 i 9 oraz wysokosci 5
+
+a=3
+b=9
+h=5
+
+wynik = 1/2 * (a + b) * h
+
+print("Pole trapezu", wynik)
+
+
+#policz objeteosc kupi o zadanym prkieniu 
